6f3b9bb6cda4354a682f1e924e710ba4ce9e38e7
Jasmina Mazej <jasmina.mazej10@gmail.com>
2024-08-07 13:04:54 +0200

    pridobivanje podatkov

--- scraping_data.py
+++ scraping_data.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import csv
+import os
 
 #sprehajamo se po današnjem dnevu
 url = 'https://www.famousbirthdays.com/'
@@ -32,20 +33,50 @@ def podatki_avgust(dan):
 #print(podatki_julij(7))
 
 def izlusci_podatke(polepsano_soup):
-    #funkcija izlusci podatke o osebah
     osebe = []
-    blocks = polepsano_soup.find_all('div', class_='tile')
+    blocks = polepsano_soup.find_all('div', class_='tile__item')
+    
+    if not blocks:
+        print("Ni najdenih 'tile' elementov.")
+    
     for block in blocks:
-        ime = block.find('a', class_='name').get_text(strip=True) if block.find('a', class_='name') else 'Ni imena'
-        poklic = block.find('p', class_='title').get_text(strip=True) if block.find('p', class_='title') else 'Ni poklica'
-        starost = block.find('p', class_='age').get_text(strip=True) if block.find('p', class_='age') else 'Ni starosti'
-        osebe.append([ime, starost, poklic])
+        ime_in_starost = block.find('p', class_='type-16-18-small').get_text(strip=True) if block.find('p', class_='type-16-18-small') else 'Ni imena'
+        poklic = block.find('p', class_='tile__description type-14-16').get_text(strip=True) if block.find('p', class_='tile__description type-14-16') else 'Ni poklica'
+        
+        if ', ' in ime_in_starost:
+            ime, starost = ime_in_starost.rsplit(', ', 1)
+        else:
+            ime = ime_in_starost
+            starost = None
+
+        #shranimo le tiste osebe, ki imajo definirano starost
+        if starost:
+            osebe.append([ime, starost, poklic])
     return osebe
 
-def shrani_v_csv(podatki, tabela_csv):
-    with open(tabela_csv, mode='w', newline='', encoding='utf-8') as file:
+def shrani_v_csv(podatki, ime_datoteke, mapa):
+    if not podatki:
+        print(f"Ni podatkov za shranjevanje v {ime_datoteke}.")
+        return
+    
+    if not os.path.exists(mapa):
+        os.makedirs(mapa)
+
+    celotna_pot = os.path.join(mapa, ime_datoteke)
+
+    with open(ime_datoteke, mode='w', newline='', encoding='utf-8') as file:
         writer = csv.writer(file)
-        writer.writerow(['Ime', 'Poklic', 'Starost'])
+        writer.writerow(['Ime', 'Starost', 'Poklic'])
         writer.writerows(podatki)
+    print(f"Podatki so shranjeni v {ime_datoteke}.")
 
+for day in range(1, 32):
+    #pridobimo podatke za julij in avgust
+    podatki_jul = podatki_julij(day)
+    podatki_avg = podatki_avgust(day)
 
+    izlusceni_jul = izlusci_podatke(podatki_jul)
+    izlusceni_avg = izlusci_podatke(podatki_avg)
+
+    shrani_v_csv(izlusceni_jul, f'famous_birthdays_july{day}.csv', 'podatki')
+    shrani_v_csv(izlusceni_avg, f'famous_birthdays_august{day}.csv', 'podatki')
